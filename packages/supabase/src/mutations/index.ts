08e876b32917d64a3fbe60f059fd5365aeea26f7
Christophe Ribeiro <christophe@ribeiro.io>
2024-10-04 10:01:14 +0200

    fix: build

--- packages/supabase/src/mutations/index.ts
+++ packages/supabase/src/mutations/index.ts
@@ -1,16 +1,13 @@
-import { logger } from "@futbiz/logger";
-import { createClient } from "@futbiz/supabase/server";
+// export async function updateUser(userId: string, data: unknown) {
+//   const supabase = createClient();
 
-export async function updateUser(userId: string, data: unknown) {
-  const supabase = createClient();
+//   try {
+//     const result = await supabase.from("users").update(data).eq("id", userId);
 
-  try {
-    const result = await supabase.from("users").update(data).eq("id", userId);
+//     return result;
+//   } catch (error) {
+//     logger.error(error);
 
-    return result;
-  } catch (error) {
-    logger.error(error);
-
-    throw error;
-  }
-}
+//     throw error;
+//   }
+// }
